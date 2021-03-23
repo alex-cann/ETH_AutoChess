b97296966c75227a028442a369fd0b6c62bfc733
alex-cann <alexwaltercann@gmail.com>
2021-03-23 15:06:48 -0400

    Update AutoChess.sol
    Added unfilled contracts

--- AutoChess.sol
+++ AutoChess.sol
@@ -1,1 +1,41 @@
+/// see CryptoKitties codebase here
+/// https://ethfiddle.com/09YbyJRfiI and here https://medium.com/loom-network/how-to-code-your-own-cryptokitties-style-game-on-ethereum-7c8ac86a4eb3
+
+
+/// connects players and arranges for who plays what games
+contract MatchMaker {
+    
+    
+}
+/// handles elo calculations
+contract RankingEngine{
+    
+    
+}
+
+
+// has all the basic data etc
+contract AutoChessBase {
+    
+    
+}
+
+/// handles the purchasing, trading and selling of units
+contract UnitMarketplace {
+    
+    
+}
+
+/// handles the generation of new units and tokens
+contract AutoChessMinter {
+    
+    
+}
+
+
+/// handles the game calculations and logic etc
+contract GameEngine {
+    
+    
+}
 
